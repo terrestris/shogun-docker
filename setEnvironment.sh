5089d87fc39dbb13ffc10602e2d40e83112fcf21
Daniel Koch <koch@terrestris.de>
2021-10-02 16:10:17 +0200

    Add GeoServer service

--- setEnvironment.sh
+++ setEnvironment.sh
@@ -4,10 +4,10 @@ CURRENT_IP=$(hostname -I | awk '{print $1}')
 
 sed -i -E "s/KEYCLOAK_HOST=(.+)/KEYCLOAK_HOST=${CURRENT_IP}/" .env
 
-#USER_ID=$(id -u)
-#GROUP_ID=$(id -g)
-#USER_NAME=$(whoami)
+USER_ID=$(id -u)
+GROUP_ID=$(id -g)
+USER_NAME=$(whoami)
 
-#sed -i "s/<<INSERT_YOUR_USER_ID>>/${USER_ID}/" .env
-#sed -i "s/<<INSERT_YOUR_USER_GROUP_ID>>/${GROUP_ID}/" .env
-#sed -i "s/<<INSERT_YOUR_USER_NAME_ID>>/${USER_NAME}/" .env
+sed -i -E "s/UID=(.+)/UID=${USER_ID}/" .env
+sed -i -E "s/GID=(.+)/GID=${GROUP_ID}/" .env
+sed -i -E "s/UNAME=(.+)/UNAME=${USER_NAME}/" .env
